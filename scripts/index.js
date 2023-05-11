16199e39091ffc508f6a67b8ea6370add27635bd
Manuel Gomez <manu11341@gmail.com>
2023-05-11 06:23:16 +0200

    feat: added a new functionality this new function check the values of the player and computer and compare them to check who is the winner

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,4 @@
+// Generate a random choice
 function getComputerChoice() {
   const choices = ['Rock', 'Paper', 'Scissor'];
   const random = Math.floor(Math.random() * choices.length);
@@ -5,7 +6,9 @@ function getComputerChoice() {
 }
 
 const computerSelection = getComputerChoice();
+console.log(computerSelection);
 
+// Get the user input value of the Prompt Windows
 function promptValue() {
   let promptValue = '';
   while (promptValue === null || promptValue === '') {
@@ -18,3 +21,34 @@ function promptValue() {
 }
 
 const playerSelection = promptValue();
+console.log(playerSelection);
+
+function playRound(playerSelection, computerSelection) {
+  let winner = '';
+
+  if (playerSelection === computerSelection) {
+    winner = `There is no winner`;
+  }
+  if (playerSelection === 'rock' && computerSelection === 'scissor') {
+    winner = 'Player Wins, Rock beats Scissor';
+  }
+  if (playerSelection === 'paper' && computerSelection === 'rock') {
+    winner = 'Player Wins, Paper beats Rock';
+  }
+  if (playerSelection === 'scissor' && computerSelection === 'paper') {
+    winner = 'Player Wins, Scissor beats Paper';
+  }
+
+  if (computerSelection === 'rock' && playerSelection === 'scissor') {
+    winner = 'Computer Wins, Rock beats Scissor';
+  }
+  if (computerSelection === 'paper' && playerSelection === 'rock') {
+    winner = 'Computer Wins, Paper beats Rock';
+  }
+  if (computerSelection === 'scissor' && playerSelection === 'paper') {
+    winner = 'Computer Wins, Scissor beats Paper';
+  }
+  return winner;
+}
+
+console.log(playRound(playerSelection, computerSelection));
